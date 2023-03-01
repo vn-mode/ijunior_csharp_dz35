17159a5e672aef3d90f7d958ce608cf20d80ae19
VITALII NAGIBIN <98809212+vn-mode@users.noreply.github.com>
2023-03-01 18:37:52 +0300

    junior_csharp_dz35

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿string exit = "exit";
+string sum = "sum";
+
+Console.Write($"Доступные вам команды:\nСумма введёных чисел - {sum}\nВыход из программы - {exit}\n");
+Console.WriteLine("Введите числа, которые хотите сложить:");
+List<int> numbers = new List<int>();
+ProcessCommands(numbers, sum, exit);
+
+static void ProcessCommands(List<int> numbers, string sum, string exit)
+{
+    bool isOpen = true;
+
+    while (isOpen)
+    {
+        string userInput = Console.ReadLine();
+
+        if (int.TryParse(userInput, out int resaultParse) != false)
+        {
+            numbers.Add(resaultParse);
+        }
+        else if (userInput.ToLower() == sum)
+        {
+            Console.WriteLine(numbers.Sum());
+        }
+        else if (userInput.ToLower() == exit)
+        {
+            isOpen = false;
+        }
+        else
+        {
+            Console.WriteLine($"Ошибка вводы. Введите число или одну из доступных вам команд.({sum} или {exit})");
+        }
+    }
+}
