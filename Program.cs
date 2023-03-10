675d8ff56812d38b77c74d1246bf16e5dbaa94e3
VITALII NAGIBIN <98809212+vn-mode@users.noreply.github.com>
2023-03-10 22:55:10 +0300

    ijunior_csharp_dz35

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
-﻿string exit = "exit";
-string sum = "sum";
+﻿string commandExit = "exit";
+string commandSum = "sum";
 
-Console.Write($"Доступные вам команды:\nСумма введёных чисел - {sum}\nВыход из программы - {exit}\n");
+Console.Write($"Доступные вам команды:\nСумма введёных чисел - {commandSum}\nВыход из программы - {commandExit}\n");
 Console.WriteLine("Введите числа, которые хотите сложить:");
 List<int> numbers = new List<int>();
-ProcessCommands(numbers, sum, exit);
+ProcessCommands(numbers, commandSum, commandExit);
 
-static void ProcessCommands(List<int> numbers, string sum, string exit)
+static void ProcessCommands(List<int> numbers, string commandSum, string commandExit)
 {
     bool isOpen = true;
 
@@ -14,21 +14,32 @@ static void ProcessCommands(List<int> numbers, string sum, string exit)
     {
         string userInput = Console.ReadLine();
 
-        if (int.TryParse(userInput, out int resaultParse) != false)
+        if (int.TryParse(userInput, out int resaultParse))
         {
             numbers.Add(resaultParse);
         }
-        else if (userInput.ToLower() == sum)
+        else if (userInput.ToLower() == commandSum)
         {
-            Console.WriteLine(numbers.Sum());
+            Console.WriteLine(SumNumbers(numbers));
         }
-        else if (userInput.ToLower() == exit)
+        else if (userInput.ToLower() == commandExit)
         {
             isOpen = false;
         }
         else
         {
-            Console.WriteLine($"Ошибка вводы. Введите число или одну из доступных вам команд.({sum} или {exit})");
+            Console.WriteLine($"Ошибка вводы. Введите число или одну из доступных вам команд.({commandSum} или {commandExit})");
         }
     }
+}
+
+static int SumNumbers(List<int> numbers)
+{
+    int numberSum = 0;
+    foreach (var number in numbers)
+    {
+        numberSum += number;
+    }
+
+    return numberSum;
 }
